9c9ff68972c714c879b64d9f8ede620d0e9901dd
FabioSimoDev <fabiofabietto182@gmail.com>
2024-02-05 17:43:35 +0100

    initial

---
+++ front-end/Capstone_Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="mx-auto px-3">
+        <h1 className="font-bold text-white">
+          PROGETTO SOCIAL - JAVA, SPRING, TAILWIND, REACT
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/test/java/simonelli/fabio/CapstoneProject/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package simonelli.fabio.CapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
