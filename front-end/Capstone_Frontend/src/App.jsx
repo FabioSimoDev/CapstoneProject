b6adca4f318adaca1b11bac6ee641a205dab2b56
FabioSimoDev <fabiofabietto182@gmail.com>
2024-02-10 02:47:18 +0100

    Login Page
    aggiunta llogin page con gestione degli errori e accesso.

--- front-end/Capstone_Frontend/src/App.jsx
+++ front-end/Capstone_Frontend/src/App.jsx
@@ -1,38 +1,13 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { login, register } from "./Redux/actions/authActions";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/LoginPage";
 
 function App() {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.auth.token);
-  useEffect(() => {
-    // dispatch(
-    //   register({
-    //     name: "Fabio",
-    //     surname: "Simonelli",
-    //     username: "FabioSimo",
-    //     email: "fabio@gmail.com",
-    //     password: "1234"
-    //   })
-    // );
-    dispatch(login({ email: "fabio@gmail.com", password: "1234" }));
-  });
-  if (token) {
-    return (
-      <>
-        {" "}
-        <h1 className="font-bold text-white">{token}</h1>
-      </>
-    );
-  }
   return (
-    <>
-      <div className="mx-auto px-3">
-        <h1 className="font-bold text-white">
-          PROGETTO SOCIAL - JAVA, SPRING, TAILWIND, REACT
-        </h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
