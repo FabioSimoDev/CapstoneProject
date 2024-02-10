5d0f423d4c04f4b39d3e35c8b94cfd017bfd27a0
FabioSimoDev <fabiofabietto182@gmail.com>
2024-02-10 19:29:22 +0100

    Test AuthController
    finiti i test sull'auth controller.

--- src/test/java/simonelli/fabio/CapstoneProject/AuthControllerTest.java
+++ src/test/java/simonelli/fabio/CapstoneProject/AuthControllerTest.java
@@ -4,41 +4,28 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import simonelli.fabio.CapstoneProject.controllers.AuthController;
-import simonelli.fabio.CapstoneProject.controllers.PostController;
+import simonelli.fabio.CapstoneProject.payloads.UserLoginDTO;
+import simonelli.fabio.CapstoneProject.payloads.UserLoginResponseDTO;
 import simonelli.fabio.CapstoneProject.payloads.UserResponseDTO;
 import simonelli.fabio.CapstoneProject.payloads.UserSignInDTO;
-import simonelli.fabio.CapstoneProject.security.JWTTools;
-import simonelli.fabio.CapstoneProject.services.PostService;
-import simonelli.fabio.CapstoneProject.services.UsersService;
 
 //@ExtendWith(SpringExtension.class)
 //@WebMvcTest(AuthController.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class PostControllerTest {
+public class AuthControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -67,6 +54,29 @@ public class PostControllerTest {
         Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
+    @Test
+    @Order(2)
+    void registerFailure() throws JsonProcessingException{
+        UserSignInDTO userRegisterDTO = generateUserRegisterDTO();
+        ResponseEntity<UserResponseDTO> response = restTemplate.postForEntity("/auth/register", userRegisterDTO, UserResponseDTO.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @Order(3)
+    void login() throws JsonProcessingException{
+        UserLoginDTO userLoginDTO = new UserLoginDTO(email, password);
+        ResponseEntity<UserLoginResponseDTO> response = restTemplate.postForEntity("/auth/login", userLoginDTO, UserLoginResponseDTO.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        authToken = response.getBody().token();
+    }
+
+    @Test
+    @Order(4)
+    void token(){
+        Assertions.assertNotNull(authToken);
+    }
+
     private UserSignInDTO generateUserRegisterDTO() {
         return new UserSignInDTO(
                 faker.name().firstName(),
@@ -76,10 +86,4 @@ public class PostControllerTest {
                 password
         );
     }
-
-//    @Test
-//    @WithMockUser
-//    public void TestGetALlPostEndpoint() throws Exception{
-//        mockMvc.perform(get("/posts")).andExpect(status().isOk()).andExpect(jsonPath("$.content", hasSize(10)));
-//    }
 }
