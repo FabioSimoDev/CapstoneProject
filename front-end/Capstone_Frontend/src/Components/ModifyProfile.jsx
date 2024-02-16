2dd5058271743009f3f1d51e9b080843ea2635c6
FabioSimoDev <fabiofabietto182@gmail.com>
2024-02-16 13:43:55 +0100

    Biografia - modifica profilo

--- front-end/Capstone_Frontend/src/Components/ModifyProfile.jsx
+++ front-end/Capstone_Frontend/src/Components/ModifyProfile.jsx
@@ -2,6 +2,9 @@ import { useSelector } from "react-redux";
 
 const ModifyProfile = () => {
   const userData = useSelector((state) => state.userData.data);
+  if (!userData) {
+    return <h1>CARICAMENTO</h1>;
+  }
   return (
     <>
       <h1 className="font-bold text-xl">Modifica profilo</h1>
@@ -22,6 +25,21 @@ const ModifyProfile = () => {
           Cambia foto
         </button>
       </div>
+      <div className="flex flex-col gap-3">
+        <p className="font-bold text-lg">Biografia</p>
+        <textarea
+          name="biography"
+          id="biographyTextArea"
+          rows="2"
+          maxLength={150}
+          className="w-full overflow-y-auto rounded-xl dark:bg-black bg-white dark:border-white/25 dark:border border-2 p-3 outline-none text-lg"
+          style={{ resize: "none" }}
+          placeholder={userData.biography ?? "Inserisci la biografia"}
+        ></textarea>
+      </div>
+      <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400">
+        Salva
+      </button>
     </>
   );
 };
